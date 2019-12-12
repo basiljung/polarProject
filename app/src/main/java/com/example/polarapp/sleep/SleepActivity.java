c1d0fa593adc9e0107cf6d76f2ecc82a9f9b82d9
Kemppis <ilari.kemppainen@live.fi>
2019-12-12 23:35:39 +0200

    database creation and updating for sleeping activity

--- app/src/main/java/com/example/polarapp/sleep/SleepActivity.java
+++ app/src/main/java/com/example/polarapp/sleep/SleepActivity.java
@@ -1,5 +1,6 @@
 package com.example.polarapp.sleep;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
@@ -13,6 +14,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
+import com.example.polarapp.preferencesmanager.ProfilePreferencesManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -26,30 +28,44 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import com.example.polarapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SleepActivity extends AppCompatActivity {
 
     private AlarmManager alarmMgr;
     private PendingIntent alarmIntent;
     public int differenceMillis;
-    boolean done = false;
+    private Timestamp startTimestamp = null; // timestamp
+    private ProfilePreferencesManager profilePreferencesManager;
+    private static final String PROFILE_USER_ID = "profile_user_id";
+    public String documentID;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
-
         final TextView timer = findViewById(R.id.timer);
-
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
         final TimePicker timePicker = findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);
 
+        profilePreferencesManager = new ProfilePreferencesManager(getBaseContext());
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,10 +95,8 @@ public class SleepActivity extends AppCompatActivity {
 
                 int differenceMillis = differenceHours*60*60*1000 + differenceMinutes * 60 * 1000;
 
-
-
+                //create sleep activity
                 startThread(differenceMillis);
-
                 scheduleNotification(getNotification("Wake Up!"), differenceMillis);
             }
         });
@@ -95,16 +109,17 @@ public class SleepActivity extends AppCompatActivity {
                 Toast.makeText(SleepActivity.this, alarmAfter, Toast.LENGTH_LONG).show();
                 int myNum = 0;
                 final int finalMyNum = myNum;
-
                 int differenceMillis = timePicker.getCurrentHour()*60*60*1000 + timePicker.getCurrentMinute() * 60 * 1000;
 
                 Log.d("yolo", "value: " + differenceMillis);
-
                 scheduleNotification(getNotification("Wake Up!"), differenceMillis);
 
                 //SleepThread sleepThread = new SleepThread(differenceMillis / 1000);
                 //sleepThread.start();
 
+                //Create sleep database
+
+                createDatabase();
                 startThread(differenceMillis);
 
                 new CountDownTimer(timePicker.getCurrentHour()*60*60*1000 + timePicker.getCurrentMinute() * 60 * 1000, 1000 * 60 * 10) {
@@ -120,7 +135,6 @@ public class SleepActivity extends AppCompatActivity {
             }
         });
     }
-
 
     private void scheduleNotification(Notification notification, int delay) {
         //Channel Set-up
@@ -152,6 +166,30 @@ public class SleepActivity extends AppCompatActivity {
         return builder.build();
     }
 
+    public void createDatabase() {
+        Log.d("yolo", "Database Created");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> sleep = new HashMap<>();
+        sleep.put("UUID", profilePreferencesManager.getStringProfileValue(PROFILE_USER_ID));
+        sleep.put("type", "sleep");
+        sleep.put("HRArray", Arrays.asList(45,55));
+
+        db.collection("activities")
+                .add(sleep)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        documentID = documentReference.getId();
+                        Log.d("yolo", "DocumentSnapshot written with ID: " + documentID);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("yolo", "Error adding document", e);
+                    }
+                });
+    }
 
     public void startThread(final int differenceMillis) {
         /*
@@ -160,26 +198,37 @@ public class SleepActivity extends AppCompatActivity {
         sleepThread.start();
         */
         //final int seconds = differenceMillis * 1000;
-
         new Thread( new Runnable() {
+
             @Override
             public void run() {
-            // Run whatever background code you want here.
-            //Thread.sleep(1000);
+
+                //FirebaseFirestore db = FirebaseFirestore.getInstance();
+                //DocumentReference sleepRef = db.collection("activities").document(documentID);
                 try {
+                    //FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                    //DocumentReference sleepRef = db.collection("activities").document(documentID);
+
                     for(int i = 0; i < differenceMillis / 1000; i++) {
                         Thread.sleep(1000);
                         Log.d("yolo", "swägä " + i);
+                        Log.d("yolo", "Sleep ID: " + documentID);
+                        if(i % 10 == 1)
+                        {
+                            DocumentReference sleepRef = db.collection("activities").document(documentID);
+                            sleepRef.update("HRArray", FieldValue.arrayUnion(i));
+                            // Atomically add a new region to the "regions" array field.
+                            //sleepRef.update("HRArray", FieldValue.arrayUnion(1));
+                            Log.d("yolo", "10 sek");
+                        }
                         //update database
-
                     }
                     Log.d("yolo", "REDI");
                     //Thread.interrupted();
                 } catch (InterruptedException e) {}
         }}).start();
     }
-
-
 
     @Override
     protected void onStart() {
